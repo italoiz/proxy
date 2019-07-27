e31c7963fd179ba47a51eaf3287f8f48163c58a9
Italo Izaac <italoiz.dev@gmail.com>
2019-07-27 00:16:03 -0300

    fix: Delete dist folder from git

--- dist/config/docker.js
+++
@@ -1,5 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }Object.defineProperty(exports, "__esModule", {value: true});var _dockerode = require('dockerode'); var _dockerode2 = _interopRequireDefault(_dockerode);
-
-exports. default = new (0, _dockerode2.default)({
-  socketPath: process.env.DOCKER_SOCK || '/app/docker.sock',
-});
